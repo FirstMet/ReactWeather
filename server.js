7578f32dcfb04b9d72d8b04cc095e7efff2c74d6
Fragments <firstmetgithub@gmail.com>
2016-10-10 14:16:33 +0800

    Update middleware for localhost

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var app = express();
 const PORT = process.env.PORT || 7777;
 
 app.use(function(req, res, next) {
-    if(req.headers['x-forwarded-proto'] === 'http') {
-        next();
-    } else {
+    if(req.headers['x-forwarded-proto'] === 'httpw') {
         res.redirect('http://' + req.hostname + req.url);
+    } else {
+        next();
     }
 });
 
