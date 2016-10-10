3d0610097b96c4127677e1211a9656e96dc553db
Fragments <firstmetgithub@gmail.com>
2016-10-10 14:02:54 +0800

    Update about & example page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 // Stateless Functional Component
 var About = (props) => {
     return (
-        <h2>About Component</h2>
+        <div>
+            <h2>About</h2>
+            <p>Welcome to about page!</p>
+        </div>
     )
 }
 
